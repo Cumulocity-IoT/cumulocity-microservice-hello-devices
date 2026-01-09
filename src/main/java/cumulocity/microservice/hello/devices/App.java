ac9e484d68787500c1bb36ae78eb56c11149b539
apes <alexander.pester@cumulocity.com>
2026-01-09 08:54:53 +0100

    initial commit

---
+++ src/main/java/cumulocity/microservice/hello/devices/App.java
@@ -0,0 +1,11 @@
+package cumulocity.microservice.hello.devices;
+
+import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
+import org.springframework.boot.SpringApplication;
+
+@MicroserviceApplication
+public class App {
+    public static void main (String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/test/java/cumulocity/microservice/hello/devices/AppTest.java
@@ -0,0 +1,15 @@
+package cumulocity.microservice.hello.devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class AppTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
